a0e04cdd278205a1d14632c4dfcb333d8a5afde7
Dark Jimmy <30280420+l3d43r@users.noreply.github.com>
2017-07-19 14:09:51 -0400

    Update 3x0scan.py

--- 3x0scan.py
+++ 3x0scan.py
@@ -1,5 +1,27 @@
 import socket
 import os
+import sys
+from colorama import Fore, Back, Style
+import time
+
+os.system("clear")
+print("\n")
+print("## Detected Hosts with open ports 22 ")
+print("<------------------------------------>")
+print("")
+
+
+
+print ("Processing wait for it...\\"),
+syms = ['\\', '|', '/', '-']
+bs = '\b'
+
+for _ in range(10):
+    for sym in syms:
+        sys.stdout.write("\b%s" % sym)
+        sys.stdout.flush()
+        time.sleep(.20)
+        sys.stdout.flush()
 
 
 def info2(ip,port):
@@ -14,12 +36,14 @@ def info2(ip,port):
 
 def main():
     port=22
-    for x in range(1,255):
-        ip="127.0.0."+str(x)
+    for x in range(74,137):
+        ip="162.246.52."+str(x)
 
         info=info2(ip,port)
         if(info):
-            print("[+]" + ip + "   --->    " + info)
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            print(Fore.LIGHTGREEN_EX+ Back.BLACK +"[+]" + ip + "   --->    " + info+Style.RESET_ALL)
             #os.system(" say Scanning , Found , open port ")
         #else:
            # print( "[+] Nothing open here -- > " + ip)
